827e953d89af77ea32111daaf151edadf1d86c36
FaeFox <92965383+FaeFox@users.noreply.github.com>
2022-03-30 02:31:21 -0700

    Create __init__.py

---
+++ timestamps/__init__.py
@@ -0,0 +1,43 @@
+import asyncio
+
+import discord
+from discord import app_commands
+from redbot.core.bot import Red
+from redbot.core.errors import CogLoadError
+
+from .commands import timestamps
+
+bot_id = None #949857998753366056
+test_guild = None #779821183285461052
+
+# add tree
+def setup(bot: Red) -> None:
+    try:
+        if not hasattr(bot, "tree"):
+            bot.tree = app_commands.CommandTree(bot)
+    except AttributeError:
+        raise CogLoadError("This cog requires at least discord.py 2.0.0a") from None
+    asyncio.create_task(_setup(bot))
+
+# add command and sync tree
+async def _setup(bot: Red):
+    assert isinstance(bot.tree, app_commands.CommandTree)
+    await bot.wait_until_red_ready()
+    assert bot.user
+    if bot.user.id == bot_id:
+        guild = discord.Object(id=test_guild)
+    else:
+        guild = None
+    bot.tree.add_command(timestamps, guild=guild)
+    await bot.tree.sync(guild=guild)
+
+# remove and resync tree
+def teardown(bot: Red):
+    if bot.user:
+        assert isinstance(bot.tree, app_commands.CommandTree)
+        if bot.user.id == bot_id:
+            guild = discord.Object(id=test_guild)
+        else:
+            guild = None
+        bot.tree.remove_command(timestamps.name, guild=guild)
+        asyncio.create_task(bot.tree.sync(guild=guild))
